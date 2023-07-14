3cc91246ae27cd474cdd72f9abca3debae5db99c
JuanDavid2221 <juanmmarucasta@gmail.com>
2023-07-14 09:34:16 -0500

    carrito

---
+++ js/carrito.js
@@ -0,0 +1,33 @@
+let  boton1 = document.getElementById("boton1");
+let pElement= document.getElementById("contador")
+let  contadores=0;
+
+boton1.onclick= function(){
+    contadores++;
+    pElement.textContent=contadores;
+}
+let  boton2 = document.getElementById("boton2");
+boton2.onclick= function(){
+    contadores++;
+    pElement.textContent=contadores;
+}
+let  boton3= document.getElementById("boton3");
+boton3.onclick= function(){
+    contadores++;
+    pElement.textContent=contadores;
+}
+let  boton4 = document.getElementById("boton4");
+boton4.onclick= function(){
+    contadores++;
+    pElement.textContent=contadores;
+}
+let  boton5 = document.getElementById("boton5");
+boton5.onclick= function(){
+    contadores++;
+    pElement.textContent=contadores;
+}
+let  boton6 = document.getElementById("boton6");
+boton6.onclick= function(){
+    contadores++;
+    pElement.textContent=contadores;
+}
